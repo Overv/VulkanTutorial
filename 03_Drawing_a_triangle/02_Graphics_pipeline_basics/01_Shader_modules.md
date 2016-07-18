7b5782a460628c3237d4de9c6f104d4d1a05a768
Alexander Overvoorde <overv161@gmail.com>
2016-07-19 00:33:01 +0200

    Add check to readFile function

--- 03_Drawing_a_triangle/02_Graphics_pipeline_basics/01_Shader_modules.md
+++ 03_Drawing_a_triangle/02_Graphics_pipeline_basics/01_Shader_modules.md
@@ -291,6 +291,10 @@ write a simple helper function to load the binary data from the files.
 
 static std::vector<char> readFile(const std::string& filename) {
     std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
 }
 ```
 
--- code/model_loading.cpp
+++ code/model_loading.cpp
@@ -1497,6 +1497,10 @@ private:
     static std::vector<char> readFile(const std::string& filename) {
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
+        if (!file.is_open()) {
+            throw std::runtime_error("failed to open file!");
+        }
+
         size_t fileSize = (size_t) file.tellg();
         std::vector<char> buffer(fileSize);
 
