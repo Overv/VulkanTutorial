53bed946770c755cb10da0b63ba722d341bc5ae9
Alexander Overvoorde <overv161@gmail.com>
2018-01-07 23:02:44 +0100

    Improve code of getRequiredExtensions function

--- code/descriptor_layout.cpp
+++ code/descriptor_layout.cpp
@@ -1074,15 +1074,11 @@ private:
     }
 
     std::vector<const char*> getRequiredExtensions() {
-        std::vector<const char*> extensions;
-
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-        for (uint32_t i = 0; i < glfwExtensionCount; i++) {
-            extensions.push_back(glfwExtensions[i]);
-        }
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
         if (enableValidationLayers) {
             extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
