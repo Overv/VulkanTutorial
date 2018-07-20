6f6261125988a1fa4da93b188354ab01f0e77956
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-07-20 18:06:07 +0200

    First draft of multisampling chapter with new images.

--- code/29_multisampling.cpp
+++ code/29_multisampling.cpp
@@ -992,6 +992,7 @@ VkSampleCountFlagBits getMaxUsableSampleCount() {
     if (counts & VK_SAMPLE_COUNT_8_BIT) { return VK_SAMPLE_COUNT_8_BIT; }
     if (counts & VK_SAMPLE_COUNT_4_BIT) { return VK_SAMPLE_COUNT_4_BIT; }
     if (counts & VK_SAMPLE_COUNT_2_BIT) { return VK_SAMPLE_COUNT_2_BIT; }
+
     return VK_SAMPLE_COUNT_1_BIT;
 }
 
