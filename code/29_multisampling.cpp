6e3e28e13edbce6a1944d4a66dbee2e0f2f16c8c
Alexander Overvoorde <overv161@gmail.com>
2020-11-29 16:42:47 +0100

    Fix maxSamplerAnisotropy limit not verified (fixes #216)

--- code/29_multisampling.cpp
+++ code/29_multisampling.cpp
@@ -994,6 +994,9 @@ VkSampleCountFlagBits getMaxUsableSampleCount() {
     }
 
     void createTextureSampler() {
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(physicalDevice, &properties);
+
         VkSamplerCreateInfo samplerInfo{};
         samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
         samplerInfo.magFilter = VK_FILTER_LINEAR;
@@ -1002,7 +1005,7 @@ VkSampleCountFlagBits getMaxUsableSampleCount() {
         samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
         samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
         samplerInfo.anisotropyEnable = VK_TRUE;
-        samplerInfo.maxAnisotropy = 16.0f;
+        samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
         samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
         samplerInfo.unnormalizedCoordinates = VK_FALSE;
         samplerInfo.compareEnable = VK_FALSE;
