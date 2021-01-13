444260e4883d547c311e55a3fb8a21a220c9a8dd
Saleh Ramezani <princearthas2012@yahoo.com>
2021-01-14 01:43:43 +0330

    Update 05_window_surface.cpp

--- code/05_window_surface.cpp
+++ code/05_window_surface.cpp
@@ -1,5 +1,8 @@
+#define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
 
 #include <iostream>
 #include <stdexcept>
