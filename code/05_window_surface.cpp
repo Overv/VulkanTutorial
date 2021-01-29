2c7ccbee1fc39dd5db6b3b8ede3f2136035c0dd8
Alexander Overvoorde <overv161@gmail.com>
2021-01-29 22:43:19 +0100

    Restore 05_window_surface.cpp

--- code/05_window_surface.cpp
+++ code/05_window_surface.cpp
@@ -1,8 +1,5 @@
-#define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>
 
 #include <iostream>
 #include <stdexcept>
