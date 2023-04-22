0b433f28c4bf9d86fc1ef3cf87a7b87016493b11
Maíra Canal <mcanal@igalia.com>
2023-04-22 18:37:05 -0300

    Use static_cast to perform casting

--- code/15_hello_triangle.cpp
+++ code/15_hello_triangle.cpp
@@ -604,8 +604,8 @@ private:
         VkViewport viewport{};
         viewport.x = 0.0f;
         viewport.y = 0.0f;
-        viewport.width = (float) swapChainExtent.width;
-        viewport.height = (float) swapChainExtent.height;
+        viewport.width = static_cast<float>(swapChainExtent.width);
+        viewport.height = static_cast<float>(swapChainExtent.height);
         viewport.minDepth = 0.0f;
         viewport.maxDepth = 1.0f;
         vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
